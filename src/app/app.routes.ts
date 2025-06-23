b3865774d439276fd457d1cf4c908cc13f672417
John Doe <johned9321@gmail.com>
2025-06-23 09:54:48 -0700

    create dashboard basic template, fetch db to populate dashboard, solved issues related to CORS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ActivitiesComponent } from './pages/activities/activities.component';
+import { MealsComponent } from './pages/meals/meals.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'meals', component: MealsComponent },
+      { path: 'activities', component: ActivitiesComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
